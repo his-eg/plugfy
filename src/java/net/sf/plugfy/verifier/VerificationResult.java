2fc889b248e1a214519764643ab9b0d025b43c27
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-01-21 21:57:11 +0100

    VerificationResult uses treeset as backend

--- src/java/net/sf/plugfy/verifier/VerificationResult.java
+++ src/java/net/sf/plugfy/verifier/VerificationResult.java
@@ -1,16 +1,17 @@
 package net.sf.plugfy.verifier;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * results of a verification run
  *
  * @author hendrik
  */
-public class VerificationResult {
+public class VerificationResult implements Iterable<String> {
 
-    private final List<String> results = new LinkedList<String>();
+    private final Set<String> results = new TreeSet<String>();
 
     /**
      * adds a result
@@ -24,6 +25,11 @@ public class VerificationResult {
     @Override
     public String toString() {
         return results.toString();
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return results.iterator();
     }
 
 }
