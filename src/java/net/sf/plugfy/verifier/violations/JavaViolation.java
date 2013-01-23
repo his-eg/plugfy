9f63f90c2b638e6406d5aa51810500d909d715d1
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-01-23 12:43:29 +0100

    properly implement compareTo and add tests for JavaViolations

--- src/java/net/sf/plugfy/verifier/violations/JavaViolation.java
+++ src/java/net/sf/plugfy/verifier/violations/JavaViolation.java
@@ -9,6 +9,7 @@
  */
 package net.sf.plugfy.verifier.violations;
 
+
 /**
  * Violation collected by the verifier represent missing  java dependencies
  * 
@@ -17,11 +18,11 @@ package net.sf.plugfy.verifier.violations;
  */
 public class JavaViolation implements Comparable<JavaViolation> {
 
-    /** name of a class missing for this violation */
-    private final String missingTypeName;
+    /** name of a required class */
+    private final String requiredType;
 
-    /** name of a missing method */
-    private final String missingMethodName;
+    /** name of a required method */
+    private final String requiredMethod;
 
     /**
      * Factory method to create a new violation object
@@ -35,57 +36,23 @@ public class JavaViolation implements Comparable<JavaViolation> {
     }
 
     private JavaViolation(final String typeName, final String methodName) {
-        this.missingTypeName = typeName;
-        this.missingMethodName = methodName;
+        this.requiredType = typeName;
+        this.requiredMethod = methodName;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (this.missingMethodName == null ? 0 : this.missingMethodName.hashCode());
-        result = prime * result + (this.missingTypeName == null ? 0 : this.missingTypeName.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (this.getClass() != obj.getClass()) {
-            return false;
-        }
-        final JavaViolation other = (JavaViolation) obj;
-        if (this.missingMethodName == null) {
-            if (other.missingMethodName != null) {
-                return false;
-            }
-        } else if (!this.missingMethodName.equals(other.missingMethodName)) {
-            return false;
-        }
-        if (this.missingTypeName == null) {
-            if (other.missingTypeName != null) {
-                return false;
-            }
-        } else if (!this.missingTypeName.equals(other.missingTypeName)) {
-            return false;
-        }
-        return true;
-    }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append("JavaViolation [");
-        if (this.missingTypeName != null) {
+        if (this.requiredType != null) {
             builder.append("missingType=");
-            builder.append(this.missingTypeName);
+            builder.append(this.requiredType);
         }
-        if (this.missingMethodName != null) {
+        if (this.requiredMethod != null) {
             builder.append(", ");
             builder.append("missingMethod=");
-            builder.append(this.missingMethodName);
+            builder.append(this.requiredMethod);
         }
         builder.append("]");
         return builder.toString();
@@ -93,8 +60,76 @@ public class JavaViolation implements Comparable<JavaViolation> {
 
     @Override
     public int compareTo(final JavaViolation o) {
+        if (o == null) {
+            throw new NullPointerException();
+        }
+        final String oRequiredType = o.requiredType;
+        final String oRequiredMethod = o.requiredMethod;
 
-        return 0;
+        //typgleichheit
+        if (this.requiredType == null) {
+            if (oRequiredType != null) {
+                return -1;
+            }
+        } else {
+            final int temp = this.requiredType.compareTo(oRequiredType);
+            if (temp != 0) {
+                return temp;
+            }
+        }
+
+        //methodengleichheit
+        if (this.requiredMethod == null) {
+            if (oRequiredMethod != null) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (oRequiredMethod != null) {
+            return this.requiredMethod.compareTo(oRequiredMethod);
+        }
+
+        return 1;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.requiredMethod == null ? 0 : this.requiredMethod.hashCode());
+        result = prime * result + (this.requiredType == null ? 0 : this.requiredType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        final JavaViolation other = (JavaViolation) obj;
+        if (this.requiredMethod == null) {
+            if (other.requiredMethod != null) {
+                return false;
+            }
+        } else if (!this.requiredMethod.equals(other.requiredMethod)) {
+            return false;
+        }
+        if (this.requiredType == null) {
+            if (other.requiredType != null) {
+                return false;
+            }
+        } else if (!this.requiredType.equals(other.requiredType)) {
+            return false;
+        }
+        return true;
     }
 
 }
