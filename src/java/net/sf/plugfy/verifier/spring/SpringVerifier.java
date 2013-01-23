6a3df0c418937dbae314de3072d25125dafd7b4a
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-01-23 20:58:54 +0100

    parse the document using the spring content handler

--- src/java/net/sf/plugfy/verifier/spring/SpringVerifier.java
+++ src/java/net/sf/plugfy/verifier/spring/SpringVerifier.java
@@ -3,6 +3,12 @@ package net.sf.plugfy.verifier.spring;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
 import net.sf.plugfy.verifier.VerificationContext;
 import net.sf.plugfy.verifier.Verifier;
 /**
@@ -14,6 +20,16 @@ public class SpringVerifier implements Verifier {
     @Override
     public void verify(String name, VerificationContext context) throws IOException {
         URL url = context.getClassLoader().getResource(name);
+        SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+        try {
+            SAXParser saxParser = parserFactory.newSAXParser();
+            SpringBeanContentHandler contentHandler = new SpringBeanContentHandler();
+            saxParser.parse(url.openStream(), contentHandler);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
 }
