fa183d6e55e809b0c7fede044605419f596082df
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-01-23 19:57:50 +0100

    empty implementation for spring verification

---
+++ src/java/net/sf/plugfy/verifier/spring/SpringVerifier.java
@@ -0,0 +1,19 @@
+package net.sf.plugfy.verifier.spring;
+
+import java.io.IOException;
+
+import net.sf.plugfy.verifier.VerificationContext;
+import net.sf.plugfy.verifier.Verifier;
+/**
+ * 
+ * @author markus
+ */
+public class SpringVerifier implements Verifier {
+
+    @Override
+    public void verify(String name, VerificationContext context) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
