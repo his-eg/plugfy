015808e705a389e499a016069f5d47fb9424c7e0
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-01-27 12:00:07 +0100

    add handling of beans to verification context

--- src/java/net/sf/plugfy/verifier/VerificationContext.java
+++ src/java/net/sf/plugfy/verifier/VerificationContext.java
@@ -13,6 +13,10 @@
 package net.sf.plugfy.verifier;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.bcel.util.Repository;
 
@@ -31,6 +35,10 @@ public class VerificationContext {
     private ClassLoader classLoader;
 
     private final URL underVerification;
+    
+    private final Map<String, String> beanDefinitions = new HashMap<String, String>();
+    
+    private final Set<String> requiredBeanIds = new HashSet<String>();
 
     /**
      * VerificationContext
@@ -97,6 +105,28 @@ public class VerificationContext {
      */
     public URL getUnderVerification() {
         return this.underVerification;
+    }
+    
+    /**
+     * Mark a bean identifier as required
+     * 
+     * @param beanId
+     */
+    public void requireBean(String beanId) {
+        if(!this.beanDefinitions.containsKey(beanId)) {
+            this.requiredBeanIds.add(beanId);
+        }
+    }
+    
+    /**
+     * Register bean definitions
+     * 
+     * @param beanId
+     * @param beanClass
+     */
+    public void registerBean(String beanId, String beanClass) {
+        this.requiredBeanIds.remove(beanId);
+        this.beanDefinitions.put(beanId, beanClass);
     }
 
 }
