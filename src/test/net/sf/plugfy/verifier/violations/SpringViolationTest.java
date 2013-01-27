393e540ae53af5dc73ae94a38b2553d9f34f69ca
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-01-27 13:59:18 +0100

    extend testing of spring violations

--- src/test/net/sf/plugfy/verifier/violations/SpringViolationTest.java
+++ src/test/net/sf/plugfy/verifier/violations/SpringViolationTest.java
@@ -7,6 +7,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 
@@ -26,13 +29,22 @@ public class SpringViolationTest {
         SpringViolation v2 = SpringViolation.create("f1", "b1", "bc1");
         SpringViolation v3 = SpringViolation.create("f1", "b1", null);
         SpringViolation v4 = SpringViolation.create("f4", "b1", "bc1");
+        SpringViolation v5 = SpringViolation.create("f1", "b2", "bc1");
         assertThat(v1, is(v1));
         assertThat(v1, is(v2));
         assertThat(Integer.valueOf(v1.hashCode()), is(Integer.valueOf(v2.hashCode())));
         assertThat(v2, is(v1));
         assertThat(Integer.valueOf(v2.hashCode()), is(Integer.valueOf(v1.hashCode())));
         assertThat(v1, not(is(v3)));
+        assertThat(v3, not(is(v1)));
         assertThat(v1, not(is(v4)));
+        
+        assertThat(v1, not(is(v5)));
+        assertThat(v5, not(is(v1)));
+        
+        assertThat(Boolean.valueOf(v1.equals(null)), is(Boolean.FALSE));
+        assertThat(Boolean.valueOf(v1.equals("")), is(Boolean.FALSE));
+        
     }
     
     /**
