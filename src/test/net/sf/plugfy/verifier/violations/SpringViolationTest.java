d317a4d1088beeb10738af67be022341ddb6dec6
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2013-01-29 08:32:03 +0100

    add factory method tests

--- src/test/net/sf/plugfy/verifier/violations/SpringViolationTest.java
+++ src/test/net/sf/plugfy/verifier/violations/SpringViolationTest.java
@@ -5,7 +5,6 @@ package net.sf.plugfy.verifier.violations;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
@@ -16,18 +15,18 @@ import org.junit.Test;
  * @author markus
  */
 public class SpringViolationTest {
-    
+
     /**
      * Test equals and hashCode
      * @throws Exception
      */
     @Test
     public void testEqualsAndHashCode() throws Exception {
-        SpringViolation v1 = SpringViolation.create("f1", "b1", "bc1");
-        SpringViolation v2 = SpringViolation.create("f1", "b1", "bc1");
-        SpringViolation v3 = SpringViolation.create("f1", "b1", null);
-        SpringViolation v4 = SpringViolation.create("f4", "b1", "bc1");
-        SpringViolation v5 = SpringViolation.create("f1", "b2", "bc1");
+        final SpringViolation v1 = SpringViolation.create("f1", "b1", "bc1");
+        final SpringViolation v2 = SpringViolation.create("f1", "b1", "bc1");
+        final SpringViolation v3 = SpringViolation.create("f1", "b1", null);
+        final SpringViolation v4 = SpringViolation.create("f4", "b1", "bc1");
+        final SpringViolation v5 = SpringViolation.create("f1", "b2", "bc1");
         assertThat(v1, is(v1));
         assertThat(v1, is(v2));
         assertThat(Integer.valueOf(v1.hashCode()), is(Integer.valueOf(v2.hashCode())));
@@ -36,15 +35,15 @@ public class SpringViolationTest {
         assertThat(v1, not(is(v3)));
         assertThat(v3, not(is(v1)));
         assertThat(v1, not(is(v4)));
-        
+
         assertThat(v1, not(is(v5)));
         assertThat(v5, not(is(v1)));
-        
+
         assertThat(Boolean.valueOf(v1.equals(null)), is(Boolean.FALSE));
         assertThat(Boolean.valueOf(v1.equals("")), is(Boolean.FALSE));
-        
+
     }
-    
+
     /**
      * Test toString
      * 
@@ -52,12 +51,12 @@ public class SpringViolationTest {
      */
     @Test
     public void testToString() throws Exception {
-        SpringViolation v1 = SpringViolation.create("f1", "b1", "bc1");
+        final SpringViolation v1 = SpringViolation.create("f1", "b1", "bc1");
         assertThat(v1.toString(), is("SpringViolation [sourceFile=f1, beanId=b1, beanClass=bc1]"));
-        SpringViolation v3 = SpringViolation.create("f1", "b1", null);
+        final SpringViolation v3 = SpringViolation.create("f1", "b1", null);
         assertThat(v3.toString(), is("SpringViolation [sourceFile=f1, beanId=b1]"));
     }
-    
+
     /**
      * Test compareTo
      * 
@@ -65,8 +64,28 @@ public class SpringViolationTest {
      */
     @Test
     public void testCompareTo() throws Exception {
-        SpringViolation v1 = SpringViolation.create("f1", "b1", "bc1");
+        final SpringViolation v1 = SpringViolation.create("f1", "b1", "bc1");
         assertThat(Integer.valueOf(v1.compareTo(v1)), is(Integer.valueOf(0)));
+    }
+
+    /**
+     * Test create
+     * 
+     * @throws Exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreate1() throws Exception {
+        SpringViolation.create(null, "id", null);
+    }
+
+    /**
+     * Test create
+     * 
+     * @throws Exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreate2() throws Exception {
+        SpringViolation.create("file", null, null);
     }
 
 }
