be5d2a653a30df28f8eb6af9e501c5fa519f1aff
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2014-01-29 10:17:41 +0100

    Extended test with a sample bean coming from the "hosting" application

--- src/test/net/sf/plugfy/verifier/spring/SpringVerifierTest.java
+++ src/test/net/sf/plugfy/verifier/spring/SpringVerifierTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertThat;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.sf.plugfy.verifier.VerificationContext;
 import net.sf.plugfy.verifier.container.JarVerifier;
@@ -45,7 +47,30 @@ public class SpringVerifierTest {
     public void testProperties() throws Exception {
         final URL url = new File("sample/sample-spring.jar").toURI().toURL();
         final ClassLoader classLoader = new URLClassLoader(new URL[] {url});
-        final VerificationContext context = new VerificationContext(new ClassLoaderRepository(classLoader), classLoader, url);
+        final Map<String, String> beans = new HashMap<String, String>();
+        beans.put("sampleBeanPreconfigured", "net.sf.plugfy.sample.SampleClass");
+        final VerificationContext context = new VerificationContext(new ClassLoaderRepository(classLoader), classLoader, url, beans);
+        new JarVerifier().verify(url, context);
+        System.out.println(context);
+        System.out.println("-------------------");
+        assertThat(context.toString(),
+                        is("VerificationContext [verified="
+                                        + url.toString()
+                                        + ", result=[SpringViolation [sourceFile=bean-config-spring.xml, beanId=missingBean, beanClass=org.springframework.samples.jpetstore.dao.ibatis.SqlMapAccountDao]], missingBeanIds=[SpringViolation [sourceFile=bean-config-spring.xml, beanId=sampleBeanMissing], SpringViolation [sourceFile=bean-config-spring.xml, beanId=missingFactory], SpringViolation [sourceFile=bean-config-spring.xml, beanId=sampleBeanMissingParent]]]"));
+    }
+
+    /**
+     * Test if missing bean ids are found
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testMissingBeanIdsWithExistingBeanContext() throws Exception {
+        final URL url = new File("sample/sample-spring.jar").toURI().toURL();
+        final ClassLoader classLoader = new URLClassLoader(new URL[] { url });
+        final Map<String, String> beans = new HashMap<String, String>();
+        beans.put("sampleBeanPreconfigured", "net.sf.plugfy.sample.SampleClass");
+        final VerificationContext context = new VerificationContext(new ClassLoaderRepository(classLoader), classLoader, url, beans);
         new JarVerifier().verify(url, context);
         System.out.println(context);
         System.out.println("-------------------");
